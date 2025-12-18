28ff534995d5a7b0a5e6cbb7230ffa0c02f49bfc
MhedhbiAli <ali.mhedhbi06@gmail.com>
2025-12-18 21:46:48 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.t', {
+    strings: ['First Year University Student', 'Junior Software Engineer', 'Web Developer'],
+    typeSpeed: 70,
+    backSpeed: 40,
+    loop: true
+});
